c90cc17866dc9d6c3e64417b310d74b9df9db960
Anthony Johnson <anthonythomasjohnson@outlook.com>
2022-01-12 20:43:56 +0900

    added routes for views for other pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root              to: 'pages#home'
   get 'pre',        to: 'pages#pre', as: :pre 
+  get 'wedding',        to: 'pages#wedding', as: :wedding 
+  get 'after',        to: 'pages#after', as: :after
+  get 'trip',        to: 'pages#trip', as: :trip
 end
